1ca2f89562a6007cff6b8a9c3d9b2e9638a47816
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-10-26 20:33:11 +0700

    video 8 category & relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,7 @@
 @section('container')
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
+    <p>By. Mala in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
     {{-- <h5>{{ $post["author"] }}</h5> --}}
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,3 +36,20 @@ Route::get('/blog', [PostController::class, 'index']);
 
 // Halaman Single Post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+// Category
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name,
+    ]);
+});
