730d7887c192eef1d61703080aabe547f9632515
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-11-03 18:15:07 +0700

    video 14 View Login & Registration

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -22,6 +22,13 @@
     </div>
 
     </ul>
+
+
+    <ul class="navbar-nav md-auto">
+      <li class="nav-item">
+        <a href="/login" class="nav-link  {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+      </li>
+    </ul>
   </div>
   </div>
 </nav>
