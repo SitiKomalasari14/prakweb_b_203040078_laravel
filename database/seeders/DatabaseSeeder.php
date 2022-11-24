0feb966c607f1087364009bf247d71ba68a97908
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-11-24 19:11:39 +0700

    video 21 Upload Image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::create([
-        //     'name' => 'Siti Komalasari',
-        //     'email' => '203040078@mail.unpas.ac.id',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Siti Komalasari',
+            'username' => 'Mala',
+            'email' => '203040078@mail.unpas.ac.id',
+            'password' => bcrypt('password')
+        ]);
 
         // User::create([
         //     'name' => 'Hifki Y',
