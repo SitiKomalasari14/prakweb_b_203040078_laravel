58da29bd763229f484a3a7afc64f5fb832652c0b
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2022-12-01 20:45:59 +0700

    video 23 Authorization

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -7,6 +7,7 @@
           Dashboard
         </a>
       </li>
+
       <li class="nav-item">
         <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
           <span data-feather="file-text" class="align-text-bottom"></span>
@@ -15,5 +16,21 @@
       </li>
     </ul>
 
+    @can('admin')
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1
+    text-muted">
+      <span>ADMINISTRATOR</span>
+    </h6>
+    <ul class="nav flex-colums">
+      <li class="nav-item">
+        <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+          <span data-feather="grid" class="align-text-bottom"></span>
+          Post Categories
+        </a>
+      </li>
+    </ul>
+    @endcan
+
+    
   </div>
 </nav>
